aa32987fbeb7fd0285f8321de71c0ff3172b64af
Mochamad Arifin <klampok.child@gmail.com>
2021-09-01 03:07:03 +0700

    Show more verbose on DB fail

--- donjo-app/views/errors/html/error_db.php
+++ donjo-app/views/errors/html/error_db.php
@@ -1,9 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <?php
+	$base_url = '/';
 	$CI =& get_instance();
-	if( ! isset($CI))
+	if( ! isset($CI) && class_exists('CI_Controller'))
 	{
 		$CI = new CI_Controller();
+	}
+	if( isset($CI) ){
+		$base_url = $CI->config->base_url();
 	}
 ?>
 
@@ -11,9 +15,9 @@
 <html lang="en">
 <head>
 	<title>Database Error</title>
-	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url()?>assets/bootstrap/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url()?>assets/css/font-awesome.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?= $CI->config->base_url()?>assets/css/AdminLTE.css" />
+	<link rel="stylesheet" type="text/css" href="<?= $base_url ?>assets/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?= $base_url ?>assets/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?= $base_url ?>assets/css/AdminLTE.css" />
 </head>
 <body>
 <div class="container">
@@ -28,13 +32,21 @@
 			<p>
 				<?= $message; ?>
 
-				Versi OpenSID <?= AmbilVersi()?>. PHP versi <?= preg_split('/-/', phpversion())[0]?>.<br><br>
+				Versi OpenSID <?= AmbilVersi()?> (<?= ENVIRONMENT ?>). PHP versi <?= preg_split('/-/', phpversion())[0]?>.<br><br>
 
 				Harap laporkan masalah ini, agar kami dapat mencarikan solusinya.
 				Untuk sementara Anda dapat kembali ke halaman <a href="<?= APP_URL ?>">awal</a>.
 			</p>
 		</div>
-
+		<?php if ( ENVIRONMENT != 'production' && isset($CI)) : ?>
+			<h3>Debug Info</h3>
+			<b>Config Path</b>
+			<pre><?= htmlentities(implode("\n", $CI->config->_config_paths )) ?></pre>
+			<b>Config Loaded</b>
+			<pre><?= htmlentities(implode("\n", $CI->config->is_loaded )) ?></pre>
+			<b>Config OpenSID Database</b>
+			<pre><?= KonfigurasiDatabase() ?></pre>
+		<?php endif ?>
 	</div>
 </div>
 </body>
