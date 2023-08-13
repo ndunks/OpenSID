d9b656144f89f549dfa0e17077dc89fc013bc3a9
Mochamad Arifin <klampok.child@gmail.com>
2023-08-13 12:20:32 +0700

    remove obsfucated code

--- donjo-app/controllers/Anjungan_pengaturan.php
+++ donjo-app/controllers/Anjungan_pengaturan.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
  *
  * File ini bagian dari:
@@ -35,33 +36,59 @@
  *
  */
 
-$__ = 'printf';
-$_  = 'Loading donjo-app/controllers/Anjungan_pengaturan.php';
+use App\Models\Galery;
+use App\Models\Kategori;
+use App\Models\SettingAplikasi;
 
-$_____              = '    b2JfZW5kX2NsZWFu';
-$______________     = 'cmV0dXJuIGV2YWwoJF8pOw==';
-$__________________ = 'X19sYW1iZGE=';
+defined('BASEPATH') || exit('No direct script access allowed');
 
-$______ = ' Z3p1bmNvbXByZXNz';
-$___    = '  b2Jfc3RhcnQ=';
-$____   = 'b2JfZ2V0X2NvbnRlbnRz';
-$__     = 'base64_decode';
-$______ = $__($______);
-if (! function_exists('__lambda')) {
-    function __lambda($sArgs, $sCode)
+class Anjungan_pengaturan extends Anjungan_Controller
+{
+    public function __construct()
     {
-        return eval("return function({$sArgs}){{$sCode}};");
+        parent::__construct();
+        $this->modul_ini     = 312;
+        $this->sub_modul_ini = 349;
     }
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    $__________________ = $__($__________________);
-$______________                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          = $__($______________);
-$__________                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              = $__________________('$_', $______________);
-$_____                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   = $__($_____);
-$____                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    = $__($____);
-$___                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     = $__($___);
-$_                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       = 'eNrtW1tzm1gSfk/V/oc8TJVnanYzgEwSVcoPQhYIrEsAcbi8pIBjgyRAxLoZ/fr9Gkm2ZMczyc7UVO0U7SiK4JzT3V9/fcGx377dy09fIFcX5f20WN1dfKo/HuTqgi+K2eI/YVn+Fi+K1f0iy27vl791itm6SMLiS3mLt9X6PizelWn5tpuFy+W7d+8uPr05HP72X2+ar+brz369IVa+/Qvl6sWVC09sL31XnAZa7+qivvTE4u+SQ+ZcvW2kkUYa+WfKRZwzgXvGWteY5LvbhaG277xq/mFfNFE19+X6SwNVI4000kgjjTTSSCON/L9J8+2MRhpppJF/rlxE4fL2/eUXfhsv+O3FpwaRRhpppJFGGvlTcv7jFNfmYtSdfvyK9+QmEW707iKx8mwZ2EoZ5fPEz9UidNW1rllpnM/fn66btJQsykaG1ak/45zOV10dlXHLyqJ6f7CJczGNpXkSaGzn28qO0/9ne3rCNVb5BbvHNTEqLDGslFXgimlI19zL/Xrz5Nyeuo207N73RiXZEk2VXdTCGZKT+FJ7frQ30LIqdB/KuFKgx5hjPexe0fpl6I2yqMD+HjcnXcUdbk/PT1PYcB16iuDbnWp43ZH1rpAMZ52Hka1cR5I4DV0501Uji6W2GOejTO9la/ha8j4TQre91rvpgvet7Xj6cRP12Qr+rQNptYk8tg494FfJ68AzNzd7vxJbU+/1HvDqW6l+rW+HEz8Z1Lr1na4qGewWIw/4aMC/Z0FvL7G0rKCzoq7iwL8pd1fpUW+8W2wGUnsbuPI8gN+DPJvfnGGIuOScsDhiRRiVoUTxzd6H7uVS748yX1IrxKWIc1UIveFS11ZZrKlzih+4sMX7liNGt+BFUMdNBs5KyrUa550P/KNcXddcmSq4Z5R6n/xRCY+Ud5Uld2XC+2AH6Q/KSHPw7/Y9OII4WTVviCewr+TdzkKfn3AA2Ie2Xg66j/yZI3bQ8ZCGrZpDe5+BbVQoqa4ZsE+FbeQjsCReYp2u1XE/4aBcBC229l3yZZtELlvDz2WNjSYSfiJyYrHnYJvOFPfXLeI9uGeIsZSR/uXBf/ihkk4Ra0jH1ncR7/5IBi6ExT4GLSaMk9M8Qm654FqeCb6bipQTIfFFO/BFs5AHquB7wz1+/W+s98ryGGvsrXi9NttBn7D3D7FxH9LowMdYIh6rW9iWAuc1neGDa9xWpr7LS/oca2zNcQ9xV2LtgbizC23FgA5w1EjrGFePPJF94HnAbQGOiMjLE8z4IiT9OT+J1zf8cOWyXq8Rf3gaT5X7wLOII/X9iHxyZbLjhU2n9crpMdt05L4tqI7ee2CTuToAj8a2rfRsNlKtXqbg3ljvGhPLMRRLUI2Jo45NnGv11LHr9Kbgm4MzTFy7MR3RwBlj1Cb6bDIHHOkZiu0sEwZdjgh9zExwBsOf8YEPjsWMic0MhXUvyaYxcx4MB3iynsoQd3XisD7ZiZqk2KhJNoNOW5lAn4L6qsLGIWx2bMei+12cRzaBYWxsVbCLccWc1udN9N5qaDrZCHYPsI45gjowncvEZJbCjnVIYJ7plIZ59IUpbHLcT/bMUeEcWXncZyt05niSZbDHUp35SrHhJ/YNbWelOMI8sR3ZGJzyuUe8HyHOPNO7nee9IzHBCa6lm3jaSXRgHLpC4mhUP1FH95z6TNyznvoC9ow2cZ/qPV8gj3TfXSamlG251qO6vR3bndW+rjq43sY54K2t2KiNG+4Zs4A4UoxQ4yzozjbRtLMI+5YQX1MdfRDBQ5G4idqP94xybR3lTBhU86NPs6ilyOBqEfbNv7Oew/eH0pfYOkb/A16PfYe3eGuQ8zW3ZfTeeIPcmFFuBN5wE7SU5SBPhcjdJpaoDHXV3x042aF8DyvCHi8tFXhf2dF5gZQJYZ9NB/loE9ntOgaOkPUGQm3XxHbMGov9OR9HXcSce9Qb1W2sPowiCXnaH8UWaiD8k8fbb9+3JVUIXA6b5x+uzVIkv0xP2Xo9cYNZYBmLDxPkPOqTXOA66vGK+sOHen7JrSyg2LrmwhAM8I+Bw5ZuVPPkrr9NAi9F/0iKSf6xng8CdySgxs0Qty14k/rSKItbwwS1cBm1eBZ045LshB0zxGqHmqVEeSnWvTOr/S5QF0WqdbrGLjnxtD9MTFf+yumeK9+Bb2veN4D/QxYDk9t61gC2fQX9A7GrlBxrZ3veKXeeBHzBxbhgM9haEp60/nFf/UIOeQbqnvl+nLXvMPfQ/FFx+HPTrbFIntYCK9TFwUReAWMxUtt1H6jvXWOu2fWq8dnZnTnyD3OT8Cv64tSTgHufLT2J+mynrV8PhfFkSbEmO9r7uXFvE3cNxAO9BfOOj3kokuqaDc4miz1fye/j3v3LQP9EH4gMqZ4R71Ab6hnKEIXjmrYO/Djq/E0XuL+C/6BgW/Q/IbD3MTv1h677bFngPYeuyqPZQ2NF1DLKWg9wOOHd+3HOM8L+tXPiuh6p9KNG6J2cP67BOQ56Z90XhacZZTwrWyHml8B50nvmyzQuEZ8tZsVZWCUFz1XMRU4x6HaKUGIy+Lv6XFgb0zMq35svbiavYaiuw3rWVNceZjfYcR+428KjuGnlLpLkO+TQjHLtpmvNqae7FT/D0UCJe/2cF9w6nrGDr4jzkHSd3O8kn22lj3zK4gK4FnyB3Kc4En+Rf7JgVNtk2CX/Hu9hHlCnkcbm9T0b907icaa/b2ToSBXyRgpd1iJMA03ErCO/zhOqD8hVOnuPW+8pzn2hrlu/k59iXO9xXudz31pgVoY/RnasMZ6E2VJsYx5FfaH42vFLXw5YEoeezgho/qLrqEXJU17m7IG7bMe7h7p3oj/I29CpzkLMyQf/EpoNDczut+DB5ynye8+v8rnt9UsdAVPkbs6fZqjJ4j2XUvQtZ2FIdE681Ltxe48hfab4HfMvjYwW7O6zzKhO9TH+nLd35st4PmLW4iXXkoXRGomHulzng5njOQC1OETvYq6Rhl2lR7ghRx65eXpuTLUDnMdrjlqP3m09j6lEzym17dQ3YEPsMegzibsROH+sbx9e4V50Hsf6RbX/sQeA66hpqyzqxqdrgI3yFc+QyDHUy/rZNZk/xlZdFk/7OL+xty9jdV4PYf9q50k0M5cpzRZGVdekX8lv6nFey0qD1vDERwuxsjLw8s4nXBEnzNGU58s/8olwRa9dGlXnuU/TwyyPunbuD2rUKtay5dl+USgHnbM6u8+j0xrUChAftnmmK/k8wXMIPWt5yh3OLpBb5/owJwXux+/y59Zti4jnFP3juT879O8yVtt4DuE71M0q8FQRc5iAuidHeF5D38u+z48R+qOVfcsPzKSEC2y5fBafw57XzkdMMUuWFG/vRNepjj84H/MPo++HpBwc+FE9yFPUNUskfT+i53HfD+vb4xFP4x/U97/heMq9P+JbLI1Qu2gWVyX0srsf4R/0yr6nF8/W/G4+Hfe8OF8VzmrhnXl1dfHpzZu//5uLV/X7z4dPv3z6ke0ne79n409PCn++oL8v/v2otvmdr+brr/ydr3PO/XxG8j3lfvn0X572JsY=';
 
-$___();
-$__________($______($__($_)));
-$________ = $____();
-$_____();
-echo $________;
+    public function index()
+    {
+        $data['form_action']      = route('anjungan_pengaturan.update');
+        $data['daftar_kategori']  = Kategori::get();
+        $data['pengaturan']       = SettingAplikasi::whereKategori('anjungan')->pluck('value', 'key')->toArray();
+        $data['anjungan_artikel'] = json_decode($data['pengaturan']['anjungan_artikel']);
+        $data['slides']           = Galery::where('parrent', 0)->where('enabled', 1)->get();
+
+        return view('admin.anjungan_pengaturan.index', $data);
+    }
+
+    public function update()
+    {
+        $this->redirect_hak_akses('u');
+
+        $data = $this->validated($this->request);
+
+        foreach ($data as $key => $value) {
+            SettingAplikasi::where('key', '=', $key)->update(['value' => $value]);
+        }
+
+        redirect_with('success', 'Berhasil Ubah Data');
+    }
+
+    protected static function validated($request = [])
+    {
+        return [
+            'anjungan_artikel'         => json_encode($request['artikel']),
+            'anjungan_teks_berjalan'   => strip_tags($request['teks_berjalan']),
+            'anjungan_profil'          => bilangan($request['tampilan_profil']),
+            'anjungan_video'           => strip_tags($request['video']),
+            'anjungan_youtube'         => strip_tags($request['youtube']),
+            'anjungan_slide'           => bilangan($request['slide']),
+            'tampilan_anjungan'        => bilangan($request['screensaver']),
+            'tampilan_anjungan_waktu'  => bilangan($request['screensaver_waktu']),
+            'tampilan_anjungan_slider' => bilangan($request['screensaver_slide']),
+            'tampilan_anjungan_video'  => strip_tags($request['screensaver_video']),
+            'anjungan_layar'           => bilangan($request['layar']),
+        ];
+    }
+}
