6219d47256e14175e8d867f443af500924566024
Mochamad Arifin <klampok.child@gmail.com>
2024-10-12 08:22:26 +0700

    add serve php

---
+++ serve.php
@@ -0,0 +1,8 @@
+<?php
+
+$url  = parse_url($_SERVER['REQUEST_URI']);
+$file = __DIR__ . $url['path'];
+
+if (is_file($file)) return false;
+
+include "index.php";
---
+++ serve.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export ENVIRONMENT=development
+php -S localhost:8080 serve.php
